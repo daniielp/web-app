6bb5259dbd968ff6eca88774f58e8e88f834420a
Daniel Pedersen <59348193+daniielp@users.noreply.github.com>
2024-10-23 18:38:03 +0200

    Update Address.jsx

--- src/components/Address/Address.jsx
+++ src/components/Address/Address.jsx
@@ -1,40 +1,40 @@
-import React from 'react'
-import {Drawer, DrawerContent, DrawerHeader, DrawerTitle, DrawerTrigger} from "../Drawer"
-import AddressInput from './AddressInput'
-import AddressLocation from './AddressLocation'
-import Slider from '../Slider'
+import React from "react";
+import {
+  Drawer,
+  DrawerContent,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "../Drawer";
+import AddressInput from "./AddressInput";
+import AddressLocation from "./AddressLocation";
+import { Slider } from "../Slider";
 
 function Address() {
   return (
     <Drawer>
-        <DrawerTrigger>
-            <button>MyAdress</button>
-        </DrawerTrigger>
-        <DrawerContent>
-            <DrawerHeader>
-                <DrawerTitle>
-                    Lokation
-                </DrawerTitle>
-            </DrawerHeader>
-            <div className='DrawerComp'>
-                <div>
-                    <AddressLocation />
-                        <div className='Drawerdivider'>
-                            
-                            <span>
-                            Eller
-                            </span>
-                            
-                        </div>
-                    <AddressInput />
-                <div>
-                <Slider />
-                </div>
+      <DrawerTrigger>
+        <button>MyAdress</button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <DrawerHeader>
+          <DrawerTitle>Lokation</DrawerTitle>
+        </DrawerHeader>
+        <div className="DrawerComp">
+          <div>
+            <AddressLocation />
+            <div className="Drawerdivider">
+              <span>Eller</span>
             </div>
+            <AddressInput />
+            <div>
+              <Slider />
             </div>
-        </DrawerContent>
+          </div>
+        </div>
+      </DrawerContent>
     </Drawer>
-  )
+  );
 }
 
-export default Address
+export default Address;
