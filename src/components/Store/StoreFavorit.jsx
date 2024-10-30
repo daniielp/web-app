81cec1b6cec7fb235eb56b78496288ea633b8e1e
Anders Rask <144682594+RaskStudio@users.noreply.github.com>
2024-10-30 11:42:58 +0100

    update

--- src/components/Store/StoreFavorit.jsx
+++ src/components/Store/StoreFavorit.jsx
@@ -1,26 +1,3 @@
-/*
-import React, { useState } from 'react';
-
-function StoreFavorit() {
-  const [isLiked, setIsLiked] = useState(false);
-  const likeToggle = () => {
-    setIsLiked(prevIsLiked => !prevIsLiked);
-  };
-
-  return (
-    <div className='likeToggleContainer' onClick={likeToggle}>
-      {isLiked ? (
-        <img src="/heart-filled.svg" alt="Heart-liked" />
-      ) : (
-        <img src="/heart.svg" alt="Heart-like" />
-      )}
-    </div>
-  );
-}
-
-export default StoreFavorit;
-*/
-
 import React from 'react';
 
 function StoreFavorit({ isFavorite, onToggle }) {
